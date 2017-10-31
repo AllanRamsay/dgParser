e4e851c4da328df1e10910bceb652dc2c478cd2f
AllanRamsay <Allan.Ramsay@manchester.ac.uk>
2017-10-31 11:57:34 +0000

    Something weird happened to my last commit
    I don't think I've added anything. Added some comments, split predicates that were doing more than one job, tidied up printing.

--- nf.pl
+++ nf.pl
@@ -1,16 +1,47 @@
 :- op(32, xfy, '\IN').
+:- op(32, xfy, '\sub').
 
 revpolarity(P0, P1) :-
     (P0 = + -> P1 = -; P1 = -).
+
+/**
+  It may be useful to order the dtrs of tree so that modifiers
+  come at the end. It's hard to see it doing any damage, unless
+  we start getting into situations in which the surface order is
+  important for dealing with pronouns.
+  **/
+orderDtrs(_, modifier(_, _)).
+
+/**
+  The tree that comes out of the parser reflects the order of
+  combination. That means that for "he loves her" we get
+
+  baseTree([[love>s, arg(dobj, *(indefinite), [woman>, modifier(A, a)])],
+            arg(subject, *(universal), [man>, modifier(B, every)])])
+
+  where we have a two-element list made of ["loves a woman", "a man"]
+
+  That's just an accident of the way the parser works. denest turns
+  this into
+
+  justRoots([love,
+             arg(dobj, *(indefinite), [woman, modifier(A, a)]),
+             arg(subject, *(universal), [man, modifier(B, every)])])
+
+  It includes sorting dtrs so that modifiers come at the end. That
+  will help when trying to deal with upward/downward matching and
+  possibly with simplification.
+  **/
 
 denest([[H | T0] | T1], D) :-
     !,
     append(T0, T1, T),
     denest([H | T], D).
-denest([H0 | T0], [H1 | T1]) :-
+denest([H0 | T0], L) :-
     !,
     denest(H0, H1),
-    denest(T0, T1).
+    denest(T0, T1),
+    qsort([H1 | T1], L, orderDtrs).
 denest(_A=B, C) :-
     !,
     denest(B, C).
@@ -22,18 +53,28 @@ denest(modifier(A, B0), modifier(A, B1)) :-
     denest(B0, B1).
 denest(X, X).
 
+/**
+  Because I have this theory of modifiers and specifiers that says
+  that any modifier *might* be a specifier and any specifier *might*
+  be a modifier, I have to do some tidying up to get rid of cases
+  where a modifier isn't actually a specifier and vice versa.
+  **/
+
 nfTree(X, X) :-
     (var(X); atomic(X)),
     !.
+%% Ignore an argument if it is marked as an identity (or is undefined)
 nfTree([_X, arg(identity, SPECX, [Y | ARGS])], N) :-
     !,
     nfTree(arg(Y, SPECX, ARGS), N).
+%% Likewise if someone has an argument whose specifier is marked as an identity (or undefined)
 nfTree([X, arg(THETAX, SPECX, [_I, arg(_SPECY, identity, D)])], N) :-
     !,
     nfTree([X, arg(THETAX, SPECX, D)], N).
 nfTree(arg(A, B, D0), arg(A, B, D1)) :-
     !,
     nfTree(D0, D1).
+%% Igore modifier nodes where the modifier is identity or undefined
 nfTree([modifier(identity, _D0) | T0], T1) :-
     !,
     nfTree(T0, T1).
@@ -52,6 +93,7 @@ nfDtrs([H0 | T0], [H1 | T1]) :-
     nfTree(H0, H1),
     nfDtrs(T0, T1).
 
+%% Get rid of affixes. 
 justRoots(X, X) :-
     (var(X); atomic(X)),
     !.
@@ -68,8 +110,7 @@ justRoots(X0, X1) :-
 
 %% qq(Q, T) means: add Q to the QSTACK and subsequently apply it to the
 %% core, leave T where it is. We will write {P:X} for P(X) because we
-%% can't write P(X). We might later convert these, but probably not because
-%% I will want to do asymmetric unification on predicates as well as terms.
+%% can't write P(X).
 qlf(X, X) :-
     (var(X); atomic(X)),
     !.
@@ -137,9 +178,40 @@ extractQQ(X0, X1, Q0, Q1) :-
 extractQQ(X0, X1, QSTACK) :-
     extractQQ(X0, X1, [], QSTACK).
 
+occursIn(X, Y) :-
+    X == Y,
+    !.
+occursIn(X, _) :-
+    (var(X); atomic(X)),
+    !,
+    fail.
+occursIn([H | T], V) :-
+    !,
+    (occursIn(H, V); occursIn(T, V)).
+occursIn(X, V) :-
+    X =.. [_ | A],
+    occursIn(A, V).
+
+checkScopes([], []).
+checkScopes([H | T0], S) :-
+    xdelete(Q, T0, T1),
+    Q = _ :: R,
+    (var(R) -> V = R; R = {_, V}),
+    occursIn(H, V),
+    !,
+    checkScopes([Q, H | T1], S).
+checkScopes([H | T0], [H | T1]) :-
+    checkScopes(T0, T1).
+
 fixQuants(X0, X2) :-
-    extractQQ(X0, X1, QSTACK),
-    applyQuants(QSTACK, X1, X2).
+    extractQQ(X0, X1, QSTACK0),
+    /*
+      If a quantifier *contains* another quantifier in its restrictor
+      then we will get problems later on. So we have to check for this
+      and re-order them as required
+      */
+    checkScopes(QSTACK0, QSTACK1),
+    applyQuants(QSTACK1, X1, X2).
 
 applyQuants([], X, X).
 applyQuants([Q0::V | QQ], X0, XN) :-
@@ -164,6 +236,16 @@ applyQuants([Q0::{R, V} | QQ], X0, XN) :-
      Q1 = Q0),
     applyQuants(QQ, X0, X1),
     XN =.. [Q1, V::{R, V}, X1].
+
+/**
+  I'm doing two things here. I'm not at all sure that I want to do
+  either of them! I'm turning things like {{A, B}, C} into {A, B,
+  C}. That's the bit that's like currying. And I'm turning things like
+  {A, B, C} in A(B, C). I'm not sure that I want to do the first of
+  these, and I'm fairly sure that I *don't* want to do the second, but
+  the way I've written it makes disentangling them awkward. A place
+  for conversation and investigation.
+  **/
 
 curry(A, A) :-
     (atomic(A); var(A)),
@@ -197,11 +279,30 @@ curryAll(H0, V, H1) :-
     curry(A0, A1),
     H1 =.. [F, V | A1].
 
+/**
+  I don't know what to do with generics. I like the version that says "John is eating peaches" means
+  
+  claim(at(ref(A, now=A),
+         (eat(#0)
+           & (dobj(#0,B)=>peach(B)
+               & subject(#0, ref(C, name(C, John)))))))
+
+  i.e. that there is some eating going on where all the things being
+  eaten are peaches, because it entails the existence of some peaches
+  without explicitly stating it. But this one needs to be worked
+  through carefully. It does seem to get a pretty reasonable
+  interpretation of "all men are fools.":
+
+  claim(at(now, man(A)=>fool(A)))
+
+  Not entirely mad about doing it as a separate step -- if this is
+  indeed the way we want to go then it should probably happen in qff.
+  **/
+
 generics(X, X) :-
     (atomic(X); var(X)),
     !.
 generics(generic(E, (THETA0, R)), THETA1 => R) :-
-    ?applyGenerics,
     !,
     THETA0 =.. [T | A],
     THETA1 =.. [T, E | A].
@@ -214,6 +315,18 @@ generics(X0, X1) :-
     generics(L0, L1),
     X1 =.. L1.
 
+/**
+  This is a place for applying forward inference rules. I think that
+  actually these should be done as actual forward inference rules,
+  because as it stands they are very dependent on the shape of the
+  tree which I'm again not mad about. The only ones we actually have
+  at the moment are about "John is a fool", "all men are fools", and
+  we've had to go for two rules to capture this, which is yuk, and
+  even with these two we will get "No man is a hero to his valet"
+  wrong
+  (http://www.thisdayinquotes.com/2010/08/no-man-is-hero-to-his-valet-backstory.html)
+  **/
+
 simplify(X, X) :-
     (var(X); atomic(X)),
     !.
@@ -221,11 +334,9 @@ simplify([STOP, X0], X1) :-
     STOP == '.',
     !,
     simplify(X0, X1).
-simplify(be(X)& (predication(X, A, xbar(v(-), n(+))) & subject(X, B)), B=A) :-
-    ?simplifyEQ,
+simplify(be(X) & (predication(X, A, xbar(v(-), n(+))) & subject(X, B)), B=A) :-
     !.
 simplify(be(X) & ((predication(X,B,xbar(v(-),n(+)))=>P0) & subject(X, A)), P1) :-
-    ?simplifyEQ,
     !,
     P0 =.. [F, B],
     P1 =.. [F, A].
@@ -251,6 +362,12 @@ simplify(X0, X1) :-
     simplify(L0, L1),
     X1 =.. L1.
 
+/**
+  Pretty standard quantifier free form: the only real oddity is that
+  we convert (P => Q, -) to (P, -_) =>(Q, -), and that we give queries
+  negative polarity.
+**/
+
 skolem(X, L) :-
     gensym('#', S),
     X =.. [S | L].
@@ -261,7 +378,7 @@ qff(X, X, _QSTACK, _POLARITY) :-
 qff(the(X :: {P0}, Q0), Q2, QSTACK, POLARITY) :-
     !,
     qff(Q0, Q1, QSTACK, POLARITY),
-    substitute(ref(X, {P0}, QSTACK), X, Q1, Q2).
+    substitute(ref(X, {P0}), X, Q1, Q2).
 qff(forall(X, P0), P2, QSTACK, +) :-
     var(X),
     !,
@@ -292,22 +409,9 @@ qff(exists(X, P0), P2, QSTACK, -) :-
 qff(exists(X :: {P0}, Q0), QFF, QSTACK, POLARITY) :-
     !,
     qff(exists(X, {P0} & Q0), QFF, QSTACK, POLARITY).
-qff(at(V, X0), QFF, QSTACK, POLARITY) :-
-    ?fixContexts,
-    !,
-    qff(X0 '\IN' V, QFF, QSTACK, POLARITY).
-qff(claim(X0), claim(X1), QSTACK, POLARITY) :-
-    ?fixContexts,
-    !,
-    qff((X0 '\IN' bel(speaker)) '\IN' now, X1, QSTACK, POLARITY).
 qff(claim(X0), claim(X1), QSTACK, POLARITY0) :-
     !,
     qff(X0, X1, QSTACK, POLARITY0).
-qff(query(X0), query(X1), QSTACK, POLARITY0) :-
-    ?fixContexts,
-    !,
-    revpolarity(POLARITY0, POLARITY1),
-    qff((X0 '\IN' bel(hearer)) '\IN' now, X1, QSTACK, POLARITY1).
 qff(query(X0), query(X1), QSTACK, POLARITY0) :-
     !,
     revpolarity(POLARITY0, POLARITY1),
@@ -333,6 +437,33 @@ qffArgs([H0 | T0], [H1 | T1], QSTACK, POLARITY) :-
 qff(X0, X1) :-
     qff(X0, X1, [], +).
 
+addContexts(X, X) :-
+    (atomic(X); var(X)),
+    !.
+addContexts(at(V, X0), X1) :-
+    !,
+    addContexts(X0 '\IN' V, X1).
+addContexts(claim(X0), claim(X1)) :-
+    !,
+    addContexts(X0 '\IN' minutes, X1).
+addContexts(query(X0), query(X1)) :-
+    !,
+    addContexts(X0 '\IN' bel(hearer), X1).
+addContexts([H0 | T0], [H1 | T1]) :-
+    !,
+    addContexts(H0, H1),
+    addContexts(T0, T1).
+addContexts(X0, X1) :-
+    X0 =.. L0,
+    addContexts(L0, L1),
+    X1 =.. L1.
+
+/**
+  Our trees are trees, i.e. nested lists. We might want to turn them
+  into conjunctions. Leaving them as they are leaves us closer to
+  natural logic, which we might want.
+  **/
+
 list2conj(X, X) :-
     (var(X); atomic(X)),
     !.
@@ -352,9 +483,21 @@ list2conjAll([H0 | T0], [H1 | T1]) :-
     list2conj(H0, H1),
     list2conjAll(T0, T1).
 
-elimEQ(X, X, _, _, _) :-
-    \+ ?elimEQ,
-    !.
+/**
+  We might at an earlier stage have done something to make a copula
+  sentence like "John is a fool" mean something like
+
+  claim(((now=#0 & fool(#2)&(ref(A,name(A,John))=#2)IN#0) IN minutes))
+
+  i.e. there is a fool, who we will call #2, and there is someone
+  called John, and these two are actually the same thing.
+
+  That was earlier. We *might* now want to actually replace #2 by John
+  -- since they're the same thing, it might make sense to have one
+  term representing them, turning this into
+  claim(fool(ref(A,name(A,John)))IN[minutes,now]). Might.
+  **/
+
 elimEQ(X, X, EQ, EQ, _POLARITY) :-
     (var(X); atomic(X)),
     !.
@@ -395,6 +538,12 @@ elimEQ(X0, X2) :-
     elimEQ(X0, X1, [], EQ, +),
     applyEQ(EQ, X1, X2).
 
+/**
+  We use X \IN C to mean that X is true in the context C. We sometimes
+  end up with things like (X \IN C0) \IN C1, which would be better as
+  X \IN C0+C2.
+  **/
+
 flattenLabels(X, X) :-
     (var(X); atomic(X)),
     !.
@@ -413,25 +562,43 @@ flattenLabels(X0, X1) :-
     flattenLabels(L0, L1),
     X1 =.. L1.
 
+/**
+  Do it all, print stuff out as you go. If you didn't do some step
+  because the flags weren't set to make you do it, don't print out the
+  intermediate result:
+
+  Flags are [qlf, fixQuants, qff, addContexts, curry, generics,
+  list2conj, simplify, elimEQ]. Some don't do much unless others have
+  already been applied, but I'm not doing anything here to ensure that
+  only a coherent set are asserted.
+
+  member(F, [qlf, fixQuants, qff, addContexts, curry, generics,
+  list2conj, simplify, elimEQ]), set(F), fail.
+  **/
+
 nf(X, FLATTENLABELS) :-
+    (?printing -> (nl, pretty(baseTree(dtree@X))); true),
     denest(dtree@X, DN),
     justRoots(DN, JR),
     (?printing -> (nl, pretty(justRoots(JR))); true),
     nfTree(JR, NF),
     (?printing -> (pretty(nf(NF))); true),
-    qlf(NF, QLF),
-    (?printing -> (nl, pretty(qlf(QLF))); true),
-    (true -> fixQuants(QLF, FQ); true),
-    (?printing -> (nl, pretty(fixQuants(FQ))); true),
-    (true -> qff(FQ, QFF); true),
-    (?printing -> (nl, pretty(qff(QFF))); true),
-    curry(QFF, CURRIED),
-    (?printing -> (nl, pretty(curried(CURRIED))); true),
-    generics(CURRIED, GENERICS),
-    (?printing -> (nl, pretty(generics(GENERICS))); true),
-    list2conj(GENERICS, LIST2CONJ),
-    (true -> simplify(LIST2CONJ, SIMP); SIMP = LIST2CONJ),
-    (?printing -> (pretty(simplify(LIST2CONJ, SIMP))); true),
-    (true -> elimEQ(SIMP, ELIMEQ); ELIMEQ = QFF),
-    (?printing -> (pretty(elimEQ(SIMP, ELIMEQ))); true),
+    (qlf -> qlf(NF, QLF); QLF = NF),
+    ((?printing, qlf) -> (nl, pretty(qlf(QLF))); true),
+    (?fixQuants -> fixQuants(QLF, FQ); FQ = QLF),
+    ((?printing, fixQuants) -> (nl, pretty(fixQuants(FQ))); true),
+    (?qff -> qff(FQ, QFF); QFF = FQ),
+    ((?printing, qff) -> (nl, pretty(qff(QFF))); true),
+    (?addContexts -> qff(QFF, CONTEXTUALISED); QFF = CONTEXTUALISED),
+    ((?printing, addContexts) -> (nl, pretty(addContexts(CONTEXTUALISED))); true),
+    (?curry -> curry(CONTEXTUALISED, CURRIED); CURRIED = QFF),
+    ((?printing, curry) -> (nl, pretty(curried(CURRIED))); true),
+    (?generics -> generics(CURRIED, GENERICS); GENERICS = CURRIED),
+    ((?printing, generics) -> (nl, pretty(generics(GENERICS))); true),
+    (?list2conj -> list2conj(GENERICS, LIST2CONJ); LIST2CONJ = GENERICS),
+    ((?printing, list2conj) -> (nl, pretty(list2conj(LIST2CONJ))); true),
+    (?simplify -> simplify(LIST2CONJ, SIMP); SIMP = LIST2CONJ),
+    ((?printing, simplify) -> (nl, pretty(simplify(LIST2CONJ, SIMP))); true),
+    (?elimEQ -> elimEQ(SIMP, ELIMEQ); ELIMEQ = QFF),
+    ((?printing, ?elimEQ) -> (nl, pretty(elimEQ(SIMP, ELIMEQ))); true),
     flattenLabels(ELIMEQ, FLATTENLABELS).
